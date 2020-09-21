4fb374d56b4e54b5523e499517c2ccc719f05a43
wykhuh <wykhuh@users.noreply.github.com>
2020-09-21 03:28:42 -0700

    add AnalyteList component

---
+++ src/components/AnalyteList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="closed">
+    <li v-for="(value, key) in list" v-bind:key="key">
+      <input type="checkbox" :id="key" :name="key" :value="key" />
+      <label :for="key">{{ key }}</label>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "AnalyteList",
+    props: ["list"],
+  };
+</script>
+
+<style scoped>
+  .closed {
+    display: none;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0 0 0 15px;
+    padding: 0;
+  }
+
+  li {
+    margin: 0;
+    padding: 0;
+  }
+</style>
--- src/components/RiverExplorer.vue
+++ src/components/RiverExplorer.vue
@@ -5,104 +5,45 @@
       <div id="controls">
         <div>
           <h2>Biodiversity</h2>
-          <ul>
-            <li @click="toggleCollapse">
-              Biological Condition
-              <ul class="closed">
-                <li>
-                  Benthic Macroinvertebrates (CSCI)
-                  <ul class="closed">
-                    <li>CSCI</li>
-                    <li>MMI</li>
-                    <li>O/E</li>
-                  </ul>
-                </li>
-                <li>
-                  Attached Algae (So CA IBI)
-                  <ul class="closed">
-                    <li>D18</li>
-                    <li>S2</li>
-                  </ul>
-                </li>
-                <li>
-                  Riparian Habitat Score (CRAM)
-                  <ul class="closed">
-                    <li>Overall CRAM Score</li>
-                    <li>Biotic Structure</li>
-                    <li>Buffer and Landscape Context</li>
-                    <li>Hydrology</li>
-                    <li>Physical Structure</li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          <div @click="toggleCollapse" class="category">
+            Biological Condition
+
+            <ul class="closed">
+              <li class="category">
+                Benthic Macroinvertebrates
+                <AnalyteList :list="Benthic_Macroinvertebrates" />
+              </li>
+              <li class="category">
+                Attached Algae
+                <AnalyteList :list="Attached_Algae" />
+              </li>
+              <li class="category">
+                Riparian Habitat Score
+                <AnalyteList :list="Riparian_Habitat_Score" />
+              </li>
+            </ul>
+          </div>
           <h2>Enviromental Conditions</h2>
-          <ul>
-            <li>
-              InSitu Measurements
-              <ul class="closed">
-                <li>Temperature (C°)</li>
-                <li>Dissolved Oxygen (mg/L)</li>
-                <li>pH</li>
-                <li>Salinity (ppt)</li>
-                <li>Specific Conductivity (us/cm)</li>
-              </ul>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              General Chemistry
-              <ul class="closed">
-                <li>Alkalinity as CaCO3 (mg/L)</li>
-                <li>Hardness as CaCO3 (mg/L)</li>
-                <li>Chloride (mg/L)</li>
-                <li>Sulfate (mg/L)</li>
-                <li>TSS (mg/L)</li>
-              </ul>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              Nutrients
-              <ul class="closed">
-                <li>Ammonia as N (mg/L)</li>
-                <li>Nitrate as N (mg/L)</li>
-                <li>Nitrite as N (mg/L)</li>
-                <li>Nitrogen Total (mg/L)</li>
-                <li>OrthoPhosphate as P (mg/L)</li>
-                <li>Phosphorus as P (mg/L)</li>
-                <li>Dissolved Organic Carbon (mg/L)</li>
-                <li>Total Organic Carbon (mg/L)</li>
-              </ul>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              Algal Biomass
-              <ul class="closed">
-                <li>AFDM (mg/cm2)</li>
-                <li>Chl-a (ug/cm2)</li>
-              </ul>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              Dissolved Metals
-              <ul class="closed">
-                <li>Arsenic (ug/L)</li>
-                <li>Cadmium (ug/L)</li>
-                <li>Chromium (ug/L)</li>
-                <li>Copper (ug/L)</li>
-                <li>Iron (ug/L)</li>
-                <li>Lead (ug/L)</li>
-                <li>Mercury (ug/L)</li>
-                <li>Nickel (ug/L)</li>
-                <li>Selenium (ug/L)</li>
-                <li>Zinc (ug/L)</li>
-              </ul>
-            </li>
-          </ul>
+          <div @click="toggleCollapse" class="category">
+            InSitu Measurements
+            <AnalyteList :list="InSitu_Measurements" />
+          </div>
+          <div @click="toggleCollapse" class="category">
+            General Chemistry
+            <AnalyteList :list="General_Chemistry" />
+          </div>
+          <div @click="toggleCollapse" class="category">
+            Nutrients
+            <AnalyteList :list="Nutrients" />
+          </div>
+          <div @click="toggleCollapse" class="category">
+            Algal Biomass
+            <AnalyteList :list="Algal_Biomass" />
+          </div>
+          <div @click="toggleCollapse" class="category">
+            Dissolved Metals
+            <AnalyteList :list="Dissolved_Metals" />
+          </div>
         </div>
       </div>
       <div id="map"></div>
@@ -111,21 +52,90 @@
 </template>
 
 <script>
-import  {initMap} from  '../js/map.js'
-
+  import { initMap } from "../js/map.js";
+  import AnalyteList from "./AnalyteList.vue";
 
   export default {
     name: "RiverExplorer",
+    components: {
+      AnalyteList,
+    },
+    data: function() {
+      return {
+        Benthic_Macroinvertebrates: {
+          CSCI: "CSCI",
+          MMI: "MMI",
+          "O/E": "O/E",
+        },
+        Attached_Algae: {
+          D18: "D18",
+          S2: "S2",
+        },
+        Riparian_Habitat_Score: {
+          "Overall CRAM Score": "Overall CRAM Score",
+          "Biotic Structure": "Biotic Structure",
+          "Buffer and Landscape Context": "Buffer and Landscape Context",
+          Hydrology: "Hydrology",
+          "Physical Structure": "Physical Structure",
+        },
+        InSitu_Measurements: {
+          "Temperature (C°)": "Temperature",
+          "Dissolved Oxygen (mg/L)": "Dissolved Oxygen",
+          pH: "pH",
+          "Salinity (ppt)": "Salinity",
+          "Specific Conductivity (us/cm)": "Specific Conductivity",
+        },
+        General_Chemistry: {
+          "Alkalinity as CaCO3 (mg/L)": "Alkalinity as CaCO3",
+          "Hardness as CaCO3 (mg/L)": "Hardness as CaCO3",
+          "Chloride (mg/L)": "Chloride",
+          "Sulfate (mg/L)": "Sulfate",
+          "TSS (mg/L)": "TSS",
+        },
+        Nutrients: {
+          "Ammonia as N (mg/L)": "Ammonia as N",
+          "Nitrate as N (mg/L)": "Nitrate as N",
+          "Nitrite as N (mg/L)": "Nitrite as N",
+          "Nitrogen Total (mg/L)": "Nitrogen Total",
+          "OrthoPhosphate as P (mg/L)": "OrthoPhosphate as P",
+          "Phosphorus as P (mg/L)": "Phosphorus as P",
+          "Dissolved Organic Carbon (mg/L)": "Dissolved Organic Carbon",
+          "Total Organic Carbon (mg/L)": "Total Organic Carbon",
+        },
+        Algal_Biomass: {
+          "AFDM (mg/cm2)": "AFDM",
+          "Chl-a (ug/cm2)": "Chl-a",
+        },
+        Dissolved_Metals: {
+          "Arsenic (ug/L)": "Arsenic",
+          "Cadmium (ug/L)": "Cadmium",
+          "Chromium (ug/L)": "Chromium",
+          "Copper (ug/L)": "Copper",
+          "Iron (ug/L)": "Iron",
+          "Lead (ug/L)": "Lead",
+          "Mercury (ug/L)": "Mercury",
+          "Nickel (ug/L)": "Nickel",
+          "Selenium (ug/L)": "Selenium",
+          "Zinc (ug/L)": "Zinc",
+        },
+      };
+    },
     methods: {
       toggleCollapse: function(event) {
-        console.log(event);
-        debugger
+        if (event.target.className != "category") return;
+
+        let className = event.target.children[0].className;
+        if (className == "closed") {
+          event.target.children[0].className = "";
+        } else {
+          event.target.children[0].className = "closed";
+        }
       },
     },
-    mounted: function () {
-      this.$nextTick(function () {
-        initMap()
-      })
+    mounted: function() {
+      this.$nextTick(function() {
+        initMap();
+      });
     },
   };
 </script>
@@ -135,7 +145,7 @@ import  {initMap} from  '../js/map.js'
     display: flex;
   }
   #controls {
-    width: 300px;
+    width: 350px;
     /* border: 1px solid green; */
     /* height: 700px; */
     /* float: left; */
@@ -143,10 +153,7 @@ import  {initMap} from  '../js/map.js'
 
   #map {
     border: 1px solid black;
-    width: 700px;
+    width: 650px;
     height: 700px;
-  }
-  .closed {
-    display: none;
   }
 </style>
